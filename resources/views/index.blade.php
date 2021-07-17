c63d9a441ea5f9f9960b1d28c0a34850482742d7
Mack Perry Co <mack.perry.co@gmail.com>
2021-07-18 02:12:07 +0800

    Fixed app icon. Fixed overflow x scroll on mobile

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,7 +14,7 @@
 	<link rel="canonical" href="https://turbostaffservices.co">
 
 	<!-- Tab Icon -->
-	<link rel="icon" href="{{ asset('img/icon.png') }}">
+	<link rel="icon" href="{{ asset('img/icon.PNG') }}">
 	<!-- Bootstrap CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/styles.css') }}">
