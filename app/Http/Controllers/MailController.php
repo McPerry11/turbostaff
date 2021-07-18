2f3dc87954c59e8382939bf4eb93cc037458f422
Mack Perry Co <mack.perry.co@gmail.com>
2021-07-18 18:26:54 +0800

    Testing mail env

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -7,12 +7,12 @@ use Illuminate\Http\Request;
 class MailController extends Controller
 {
   public function mail() {
-    $to = '';
+    $to = 'book@turbostaffservices.co';
     $subject = 'Test Mail';
     $message = 'Testing testing';
-    $from = '';
+    $from = 'comackperry@yahoo.com';
     $headers = 'From: ' . $from;
-    // mail($to, $subject, $message, $headers);
+    mail($to, $subject, $message, $headers);
     // Temporarily disabled mail function (doesn't work on GoDaddy)
     return response()->json([
       'status' => 'error',
